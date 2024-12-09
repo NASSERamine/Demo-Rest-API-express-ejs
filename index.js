67a104ec7854cb998c5f1350237b3b4e8d63864c
NASSERamine <nassermedamine21@gmail.com>
2024-12-09 16:21:23 +0100

    commit

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express= require("express")
-const bodyParser=require("body-parser")
-const app= express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-var urlencodedParser=bodyParser.urlencoded({extended:false})
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  res.send("ya hala bl 8ali");
+});
 
-app.get("/",(req,res)=>{
-    res.send("ya hala bl 8ali")
-})
+app.post("/login", urlencodedParser, function (req, res) {
+  res.send("  hello   " + req.body.username);
+});
 
-app.post("/login",urlencodedParser, function(req,res){
-    res.send("  hello   "+req.body.username)
-
-})
-
-app.listen(8081,()=>{
-    console.log("app is listening on port 8081")
-})
+app.listen(8081, () => {
+  console.log("app is listening on port 8081");
+});
